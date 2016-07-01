a72dbeee52acb3d440e30627f21c99fcb99957a0
Monstrofil <shalal545@gmail.com>
2016-07-01 21:02:12 +0300

    changed build script

--- ml-world-of-warships-as3/build.sh
+++ ml-world-of-warships-as3/build.sh
@@ -4,4 +4,4 @@ set -e
 python prebuild.py
 /home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsHammerConfigUbuntu.xml -debug=true -incremental=true -swf-version=12 -o ../build/gui/flash/XVMBackground.swf
 cp -a configs/. ../build/gui/flash/
-rm ../build/gui/flash/XVMBackground.swf.cache
+rm $WORKSPACE/build/gui/flash/XVMBackground.swf.cache
