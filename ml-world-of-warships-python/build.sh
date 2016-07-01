3084e3803284581a2aff261a83008312290821b9
Monstrofil <shalal545@gmail.com>
2016-07-01 20:50:54 +0300

    workspace changed

--- ml-world-of-warships-python/build.sh
+++ ml-world-of-warships-python/build.sh
@@ -1,5 +1,5 @@
 cp -R ml_client tempdirectory
 python prebuild.py
-rm -R $WORKSPACE../build/scripts/client
-cp -R tempdirectory $WORKSPACE../build/scripts/client
+rm -R $WORKSPACE/build/scripts/client
+cp -R tempdirectory $WORKSPACE/build/scripts/client
 rm -R tempdirectory
