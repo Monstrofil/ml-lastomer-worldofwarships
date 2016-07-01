9799dc45729ca5793c9c13ba86a5150547453edb
Monstrofil <shalal545@gmail.com>
2016-07-01 21:04:38 +0300

    fixed path

--- ml-world-of-warships-as3/build.sh
+++ ml-world-of-warships-as3/build.sh
@@ -3,5 +3,5 @@ set -e
 
 python prebuild.py
 /home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsHammerConfigUbuntu.xml -debug=true -incremental=true -swf-version=12 -o ../build/gui/flash/XVMBackground.swf
-cp -a configs/. ../build/gui/flash/
+cp -a configs/. $WORKSPACE/build/gui/flash/
 rm $WORKSPACE/build/gui/flash/XVMBackground.swf.cache
