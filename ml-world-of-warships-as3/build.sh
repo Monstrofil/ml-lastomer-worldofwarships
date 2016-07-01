63cbbc916851b1d52ce7c05ee3a52eea781f39a2
Monstrofil <shalal545@gmail.com>
2016-07-01 22:29:15 +0300

    changed config location

--- ml-world-of-warships-as3/build.sh
+++ ml-world-of-warships-as3/build.sh
@@ -2,4 +2,4 @@
 
 python prebuild.py
 /home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsHammerConfigUbuntu.xml -debug=true -incremental=true -swf-version=12 -o $WORKSPACE/build/gui/flash/XVMBackground.swf
-cp -a configs/. $WORKSPACE/build/gui/flash/
+cp -a configs/. $WORKSPACE/build/
