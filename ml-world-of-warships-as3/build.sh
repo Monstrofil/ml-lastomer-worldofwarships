dc41436b8fc31d0707a32f835ded1c5db5b72ba3
Monstrofil <shalal545@gmail.com>
2016-07-01 21:29:04 +0300

    changeg path to $WORKSPACE

--- ml-world-of-warships-as3/build.sh
+++ ml-world-of-warships-as3/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python prebuild.py
-/home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsHammerConfigUbuntu.xml -debug=true -incremental=true -swf-version=12 -o ../build/gui/flash/XVMBackground.swf
-cp -a configs/. ../build/gui/flash/
+/home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsHammerConfigUbuntu.xml -debug=true -incremental=true -swf-version=12 -o $WORKSPACE/build/gui/flash/XVMBackground.swf
+cp -a configs/. $WORKSPACE/build/gui/flash/
