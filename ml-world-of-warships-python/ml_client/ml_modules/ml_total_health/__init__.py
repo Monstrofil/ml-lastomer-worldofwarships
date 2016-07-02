83668a497715f1dbc71530345c31298502b1c416
Monstrofil <shalal545@gmail.com>
2016-07-02 15:13:47 +0300

    added total health script

---
+++ ml-world-of-warships-python/ml_client/ml_modules/ml_total_health/__init__.py
@@ -0,0 +1,65 @@
+__author__ = 'shish'
+import Vehicle, Avatar
+
+import ml_tools.ClientModules, sys, BWPersonality
+PlayersInfo = sys.modules['ml_tools'].ClientModules.PlayersInfo
+
+import ml_xvp.events
+registerEvent = sys.modules['ml_xvp'].events.registerEvent
+
+class TeamsData(object):
+    def __init__(self):
+        self.alliedVehicles = {}
+        self.enemyVehicles = {}
+        self.alliesHealth = 0
+        self.enemiesHealth = 0
+
+        self.vehicles = {}
+
+    def clear(self):
+        self.alliedVehicles = {}
+        self.enemyVehicles = {}
+        self.alliesHealth = 0
+        self.enemiesHealth = 0
+
+    def onVehicleChangedHealth(self, avatarId, health, isAlly):
+        storage = self.alliedVehicles if isAlly else self.enemyVehicles
+
+        if avatarId in storage:
+            if isAlly:
+                self.alliesHealth -= storage[avatarId]
+            else:
+                self.enemiesHealth -= storage[avatarId]
+        storage[avatarId] = health
+
+        if isAlly:
+            self.alliesHealth += storage[avatarId]
+        else:
+            self.enemiesHealth += storage[avatarId]
+
+        self.update()
+
+    def update(self):
+        if BWPersonality.uiManager._UIManager__mainContext.battle:
+            BWPersonality.uiManager._UIManager__mainContext.battle.infoHolder.call("ML_onTeamsHpChanged", [self.alliesHealth, self.enemiesHealth])
+gTeamsData = TeamsData()
+
+def onPlayersListUpdated(*args, **params):
+    for player in PlayersInfo.iterPlayers():
+        gTeamsData.onVehicleChangedHealth(player.avatarId, player.maxHealth, player.teamId==PlayersInfo.getSelfPlayerInfo().teamId)
+PlayersInfo.gPlayersListUpdated += onPlayersListUpdated
+
+old_set_health = Vehicle.Vehicle.set_health
+def new_set_health(self, oldValue):
+    PlayersInfo.gPlayersListUpdated -= onPlayersListUpdated
+    vTeamId = PlayersInfo.getPlayerByVehicleId(self.id).teamId
+    mTeamId = PlayersInfo.getSelfPlayerInfo().teamId
+
+    gTeamsData.onVehicleChangedHealth(self.owner, self.health, vTeamId == mTeamId)
+    old_set_health(self, oldValue)
+Vehicle.Vehicle.set_health = new_set_health
+
+
+def onPreBattleStarted(*args, **params):
+    gTeamsData.clear()
+Avatar.evEnterPreBattle += onPreBattleStarted()
