e2e9c052ff594e50b87cdc3184850f755769eedb
Monstrofil <shalal545@gmail.com>
2016-07-04 18:00:50 +0300

    omit-trace-statements enabled

--- ml-world-of-warships-as3/build.sh
+++ ml-world-of-warships-as3/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python prebuild.py
-/home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsHammerConfigUbuntu.xml -debug=true -incremental=true -swf-version=12 -o $WORKSPACE/build/gui/flash/XVMBackground.swf
+/home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsHammerConfigUbuntu.xml -omit-trace-statements -debug=false -incremental=true -swf-version=12 -o $WORKSPACE/build/gui/flash/XVMBackground.swf
 cp -a configs/. $WORKSPACE/build/
