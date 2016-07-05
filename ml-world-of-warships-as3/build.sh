567d48c27ed725831884611187fe20105faa6c98
Monstrofil <shalal545@gmail.com>
2016-07-05 12:34:28 +0300

    buildscript changed

--- ml-world-of-warships-as3/build.sh
+++ ml-world-of-warships-as3/build.sh
@@ -2,6 +2,6 @@
 
 python prebuild.py
 /home/monstrofil/dev/flash/flex/bin/mxmlc -load-config+=WorldofWarshipsHammerConfigUbuntu.xml -omit-trace-statements -debug=false -incremental=true -swf-version=12 -o $WORKSPACE/build/gui/flash/XVMBackground.swf
-/usr/bin/python /home/monstrofil/dev/xml2as/xml2as.py.pyc -i $WORKSPACE/configs/gui/unbound/dock.xml -n $WORKSPACE/MLDock -f /home/monstrofil/dev/flash/flex/bin/mxmlc
-/home/monstrofil/dev/flash/flex/bin/mxmlc $WORKSPACE/MLDock.as -compatibility-version 4.12.1 -output $WORKSPACE/build/gui/flash/MLDock.swf
+/usr/bin/python /home/monstrofil/dev/xml2as/xml2as.py.pyc -i $WORKSPACE/ml-world-of-warships-as3/configs/gui/unbound/dock.xml -n $WORKSPACE/ml-world-of-warships-as3/MLDock -f /home/monstrofil/dev/flash/flex/bin/mxmlc
+/home/monstrofil/dev/flash/flex/bin/mxmlc $WORKSPACE/ml-world-of-warships-as3/MLDock.as -compatibility-version 4.12.1 -output $WORKSPACE/build/gui/flash/MLDock.swf
 cp -a configs/. $WORKSPACE/build/
