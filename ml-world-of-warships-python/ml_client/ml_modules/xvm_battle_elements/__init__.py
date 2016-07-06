c2501832f7ad2fabc92990e2629ea90380293635
Monstrofil <shalal545@gmail.com>
2016-07-06 19:11:36 +0300

    battle_elements changed

--- ml-world-of-warships-python/ml_client/ml_modules/xvm_battle_elements/__init__.py
+++ ml-world-of-warships-python/ml_client/ml_modules/xvm_battle_elements/__init__.py
@@ -18,7 +18,8 @@ finally:
     content = f.read()
     f.close()
     if content.find('<element name="XVMBackground" class="XVMBackground" url="XVMBackground.swf"/>') == -1:
-        content = content.replace('<elementList>', '<elementList><element name="XVMBackground" class="XVMBackground" url="XVMBackground.swf"/>')
+        content = content.replace('</elementList>', '''<element name="XVMBackground" class="XVMBackground" url="XVMBackground.swf"/><element name="markersContainer" class="flash.display.Sprite" url="gui_slots.swf"><properties x='0' y='0'/><properties hitTest = "true"/></element></elementList>''')
+        content = content.replace('<element name="markersContainer" class="flash.display.Sprite" url="gui_slots.swf">', '<element name="nullelemenet" class="flash.display.Sprite" url="XVMBackground.swf">')
     if content.find('<controller class="XVMDatahubController" clips="XVMBackground"/>') == -1:
         content = content.replace('<controllers>', '<controllers><controller class="XVMDatahubController" clips="XVMBackground"/>')
     f = open('../' + resModPath.asString + '/gui/battle_elements.xml', 'wb')
