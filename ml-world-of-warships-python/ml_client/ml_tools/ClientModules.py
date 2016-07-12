e9b5bec84ee0f25bf7059ec3a4c708d44ab8532a
Monstrofil <shalal545@gmail.com>
2016-07-12 20:33:05 +0300

    0.5.8.1

--- ml-world-of-warships-python/ml_client/ml_tools/ClientModules.py
+++ ml-world-of-warships-python/ml_client/ml_tools/ClientModules.py
@@ -1,26 +1,16 @@
 __author__ = 'Monstrofil'
-# try: #replace scripts when radio will appear 0.5.6
-#     import maf3281b as PlayersInfo
-#     import m8a0249c.mbcac6ce as i18n
-#     from gui.Scaleform.m84b268f import m84b268f as VehicleStateController
-# except:
-#     import m28d6a54 as PlayersInfo
-#     import m5946a33.mcedc913 as i18n
-#     from gui.Scaleform.mc62a2ae import mc62a2ae as VehicleStateController
-
 # # 0.5.5.1
 # import m8898246 as PlayersInfo
 # import m2cf8043.m39ea978 as i18n
 # from gui.Scaleform.m644c896 import m644c896 as VehicleStateController
 
-try: #replace scripts when radio will appear 0.5.8
+try: #replace scripts when radio will appear 0.5.8.1
     import mf2b6807 as PlayersInfo
     import m96e062f.m43140dc as i18n
     from gui.Scaleform.mfbee238 import mfbee238 as VehicleStateController
 except:
-    import mec5416a as PlayersInfo
-    import m49338dd.ma83fcdc as i18n
-    from gui.Scaleform.md7d85dd import md7d85dd as VehicleStateController
+    import m590a2cf as PlayersInfo
+    import m57846e3.m492643a as i18n
+    from gui.Scaleform.m7eae24b import m7eae24b as VehicleStateController
 
-#058
-#DialogManager: m944c932
+
